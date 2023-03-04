3f40ddb27e7fa56ec7fade0382dc20c306ffc93b
Gabriel Yasuda <gabrielyyasuda@gmail.com>
2023-03-04 11:13:43 -0300

    Classes

--- 04-classes/01-class-definition/Customer.ts
+++ 04-classes/01-class-definition/Customer.ts
@@ -1,11 +1,27 @@
 class Customer {
-  firstName: string;
-  lastName: string;
+  private firstName: string;
+  private lastName: string;
 
   //Constructor
-  constructor(first: string, last: string) {
+  public constructor(first: string, last: string) {
     this.firstName = first;
     this.lastName = last;
+  }
+
+  public getFirstName(): string {
+    return this.firstName;
+  }
+
+  public setFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
+
+  public getLastName(): string {
+    return this.lastName;
+  }
+
+  public setLastName(lastName: string): void {
+    this.lastName = lastName;
   }
 }
 
