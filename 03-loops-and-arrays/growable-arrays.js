587555cfb3ea9645e11528110a60ba91cdeeaf04
Gabriel Yasuda <gabrielyyasuda@gmail.com>
2023-03-04 10:55:30 -0300

    Commit

---
+++ 03-loops-and-arrays/growable-arrays.js
@@ -0,0 +1,7 @@
+var sportArrays = ["Golf", "Cricket", "Tennis", "Jiu Jitsu"];
+sportArrays.push("Judo");
+sportArrays.push("Swimming");
+for (var _i = 0, sportArrays_1 = sportArrays; _i < sportArrays_1.length; _i++) {
+    var sport = sportArrays_1[_i];
+    console.log(sport);
+}
---
+++ 03-loops-and-arrays/growable-arrays.ts
@@ -0,0 +1,8 @@
+let sportArrays: string[] = ["Golf", "Cricket", "Tennis", "Jiu Jitsu"];
+
+sportArrays.push("Judo");
+sportArrays.push("Swimming");
+
+for (let sport of sportArrays) {
+  console.log(sport);
+}
